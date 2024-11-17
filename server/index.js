78de8cf3ada60076e8fed7209b7810f94e95dd5b
Konstantin Kosolapov <kkosolapov@gmail.com>
2024-11-17 15:23:57 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 
 const PORT = 5000;
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
 });
