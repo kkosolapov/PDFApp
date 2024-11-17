a20b3f57910680cc24aa5ac9ed8d1a3c1df23ab9
Konstantin Kosolapov <kkosolapov@gmail.com>
2024-11-17 13:40:20 +0200

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+});
